df741d68360de759f98de48673ffb06c70a2c37f
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-05-26 18:54:59 -0500

    Ajuste

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -53,7 +53,7 @@
     
         figure.mb-5(data-aos="fade-up")
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/UPDQ7P_fhlU" title="Clasificación según forma del pie" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .caja-titulos-tercer-nivel.mb-5(data-aos="fade-right")
       .col-lg-auto
