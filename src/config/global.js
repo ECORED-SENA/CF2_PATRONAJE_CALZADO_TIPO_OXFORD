603918511a275b8ccd550555a023484fdfa6294e
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-08-12 14:25:27 -0500

    Ajuste

--- src/config/global.js
+++ src/config/global.js
@@ -288,7 +288,7 @@ export default {
       ],
     },
     {
-      titulo: 'GESTORES DE REPOSITORIO',
+      titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
           nombre: 'Aixa Natalia Sendoya Fernández',
